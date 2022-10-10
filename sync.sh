cf3b02f180f47393e84806f2dc49f198dfa9dfc9
Anton Skuratovich <iantonio.by@gmail.com>
2022-10-10 16:05:59 +0300

    Change script

--- sync.sh
+++ sync.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+if ! [ -d /gh-pages ]; then
 mkdir -p gh-pages
+fi
 cd gh-pages
 helm package ../charts/wordpress
 helm repo index --url https://tony-by.github.io/wordpress-ci/ --merge index.yaml .
