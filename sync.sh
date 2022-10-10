959a0d8786a04811a35dd429e1064f8dd3af0370
Anton Skuratovich <iantonio.by@gmail.com>
2022-10-10 16:27:53 +0300

    Test 3

--- sync.sh
+++ sync.sh
@@ -4,4 +4,4 @@ mkdir -p gh-pages
 fi
 cd gh-pages
 helm package ../charts/wordpress
-helm repo index --url https://tony-by.github.io/wordpress-ci/ --merge index.yaml .
+#helm repo index --url https://tony-by.github.io/wordpress-ci/ --merge index.yaml .
